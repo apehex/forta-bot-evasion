672f8284f76c73b2fcde5bff8a5e1647e071dabe
apehex <apehex@protonmail.com>
2023-11-22 12:15:28 +0100

    Disable the ENV variables that are not used + remove obsolete code

--- src/agent.py
+++ src/agent.py
@@ -30,7 +30,7 @@ PROVIDER = Web3(Web3.HTTPProvider(get_json_rpc_url()))
 # INIT ########################################################################
 
 forta_toolkit.logging.setup_logger(logging.INFO)
-forta_toolkit.parsing.env.load_secrets()
+# forta_toolkit.parsing.env.load_secrets()
 
 def initialize():
     """Initialize the state variables that are tracked across tx and blocks."""
@@ -93,49 +93,3 @@ def handle_transaction_factory(
 
 # run with the default settings
 handle_transaction = handle_transaction_factory(provider=PROVIDER)
-
-# TODO ########################################################################
-
-# import forta_toolkit.parsing.transaction
-
-# import ioseeth.metrics.evasion.morphing.logic_bomb
-# import ioseeth.metrics.evasion.redirection
-
-# is_hidden_proxy = functools.lru_cache(maxsize=128)(ioseeth.metrics.evasion.redirection.is_hidden_proxy)
-# is_red_pill = functools.lru_cache(maxsize=128)(ioseeth.metrics.evasion.morphing.logic_bomb.is_red_pill)
-
-# def handle_transaction_factory(
-#     provider: Web3,
-#     min_confidence: float=src.options.MIN_CONFIDENCE,
-#     history_size: int=src.options.ALERT_HISTORY_SIZE
-# ) -> callable:
-#     """Setup the main handler."""
-#     @forta_toolkit.profiling.timeit
-#     @forta_toolkit.alerts.alert_history(size=history_size)
-#     def __handle_transaction(log: TransactionEvent) -> list:
-#         """Main function called on the logs gathered by the Forta network."""
-#         global CHAIN_ID
-#         # result: list of alerts
-#         __findings = []
-#         __data = forta_toolkit.parsing.transaction.parse_transaction_data(transaction=log.transaction)
-#         # analyse the transaction
-#         __scores = score(**__data)
-#         # metamorphic contracts
-#         hidden proxy
-#         if __scores['hidden-proxy'] >= min_confidence:
-#             __findings.append(src.findings.FormatFindingHiddenProxy(
-#                 chain=CHAIN_ID,
-#                 txhash=log.transaction.hash,
-#                 sender=__data['sender'],
-#                 recipient=__data['recipient'],
-#                 confidence=__scores['hidden-proxy']))
-#         # red pill
-#         if __scores['red-pill'] >= min_confidence:
-#             __findings.append(src.findings.FormatFindingRedPill(
-#                 chain=CHAIN_ID,
-#                 txhash=log.transaction.hash,
-#                 sender=__data['sender'],
-#                 recipient=__data['recipient'],
-#                 confidence=__scores['red-pill']))
-#         return __findings
-#     return __handle_transaction
